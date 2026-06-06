c863dd0cf6dd3b19d4951811241f9c8ca2a423be
Raginideekshit <raginim678@gmail.com>
2026-06-06 11:38:57 +0530

    New Code added in Demo2 Class

--- src/ragini_demo/Demo2.java
+++ src/ragini_demo/Demo2.java
@@ -1,11 +1,22 @@
 package ragini_demo;
 
+import java.util.Scanner;
+
 public class Demo2 {
+
+	public static int sumOfAdd(int num1,int num2) { //methodcreate parameter
+		int sum=num1+num2;
+		return sum;
+	}
 	public static void main(String[] args) {
-		int a = 10;
-		int b = 20;
-		int c = a + b;
-		System.out.println("The sum of a and b is: " + c);
+		Scanner scan= new Scanner(System.in);
+		System.out.println("Enter scan value 1");
+		int scanValue1 = scan.nextInt();
+		System.out.println("Enter scan value 2");
+		int scanValue2 = scan.nextInt();
+		
+		int storeRetrunValue =sumOfAdd(scanValue1,scanValue2);
+		System.out.println("Sum of "+ scanValue1 +" and "+scanValue2+" is "+storeRetrunValue);//input message +
 	}
 
 }
