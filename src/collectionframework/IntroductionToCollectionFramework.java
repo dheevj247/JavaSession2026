eadf91beb458fb59de0b03435eef78454723e7ad
Dheeraj V <128276207+dheevj247@users.noreply.github.com>
2026-07-09 09:03:05 +0530

    collection
    Code For Complietimepolymorphism

---
+++ src/collectionframework/IntroductionToCollectionFramework.java
@@ -0,0 +1,11 @@
+package collectionframework;
+
+public class IntroductionToCollectionFramework {
+	// Collection Framework: it is group of ready made hierarchy (class and interface) 
+	// which is used to store and manipulate multiple data in java.
+	
+	// Collection Framework is divided into 3 parts:
+	// 1. Collection Interface (List, Set, Queue)
+	// 2. Map Interface (HashMap, TreeMap, LinkedHashMap)
+	// 3. Legacy Classes (Vector, Stack, Hashtable)
+}
